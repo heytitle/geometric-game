db3e14c83ac8b977d9dc81221c2990e1b7ca7791
heytitle <pat.chormai@gmail.com>
2015-12-21 16:40:59 +0100

    Scoreboard

--- app/scripts/main2.js
+++ app/scripts/main2.js
@@ -11,17 +11,44 @@ var STATE = {
 var gameControl = {
     state: STATE.IDLE,
     selectingDuration: 1000,
-    waitingDuration: 1000
+    waitingDuration: 1000,
+    gameDuration: 180, // 3 Mins
+    score: 0,
+    computeScore: function(){
+        /* Compute Here */
+        var score = 1000;
+        /* End compute */
+
+        this.score += score;
+        document.getElementById('scoreboard').innerHTML = this.score;
+    }
 };
 
 var paper   = Snap(IDCANVAS);
 var sepLine = paper.line(0,0,0,600);
 sepLine.addClass('separate-line');
+document.getElementById('time').innerHTML = gameControl.gameDuration;
 
 /* Separate line Handling */
 paper.drag(function(dx,dy,x,y){
     /* Move Event */
-    console.log(dx,dy);
+    if( gameControl.state != STATE.IDLE  && gameControl.state != STATE.SELECTING ) {
+        console.log('Please wait a little bit.');
+        return;
+    }else {
+        sepLine.addClass('show');
+        gameControl.state = STATE.SELECTING;
+        sepLine.animate({ opacity:0 }, gameControl.selectingDuration, function(){
+            sepLine.removeClass('show');
+            sepLine.attr('opacity',1);
+            gameControl.state = STATE.WAITING;
+
+            setTimeout(function(){
+                gameControl.state = STATE.IDLE;
+            }, gameControl.waitingDuration );
+        });
+    }
+
     var endpoints = [0,600];
     for( var i = 0; i < endpoints.length; i++ ){
         var attrKey = 'x'+(i+1);
@@ -29,25 +56,8 @@ paper.drag(function(dx,dy,x,y){
         sepLine.attr(attrKey, endpoints[i]);
     }
 },function(x,y){
-    if( gameControl.state != STATE.IDLE ) {
-        console.log('Please wait a little bit.');
-        return;
-    }
-    console.log('click');
-    sepLine.addClass('show');
-    gameControl.state = STATE.SELECTING;
     sepLine.originX = x;
     sepLine.originY = y;
-
-    sepLine.animate({ opacity:0 }, gameControl.selectingDuration, function(){
-        sepLine.removeClass('show');
-        sepLine.attr('opacity',1);
-        gameControl.state = STATE.WAITING;
-
-        setTimeout(function(){
-            gameControl.state = STATE.IDLE;
-        }, gameControl.waitingDuration );
-    });
 });
 
 /* Objects */
@@ -68,6 +78,8 @@ for( var i=0; i < numObjects; i++ ){
 }
 
 setInterval(function(){
+    if( gameControl.state == STATE.TIMEUP ) return;
+
     var speedRatio = 0.5;
     if( gameControl.state == STATE.SELECTING ) {
         speedRatio = 0.02;
@@ -80,3 +92,18 @@ setInterval(function(){
         );
     }
 }, 50);
+
+/* Timer */
+setInterval(function(){
+    var timeLeft = gameControl.gameDuration--;
+    document.getElementById('time').innerHTML = timeLeft;
+    if( timeLeft == 0 ) {
+        gameControl.state = state.TIMEUP;
+    }
+}, 1000 );
+
+/* Capture Key */
+window.addEventListener("keypress", function(e){
+     if (e.charCode != "32" || gameControl.state != STATE.SELECTING ) return;
+     gameControl.computeScore();
+}, false);
