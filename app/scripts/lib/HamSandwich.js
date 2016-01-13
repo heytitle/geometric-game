edbfd87dc2777c4fa386910db60f0dfa1392ae6f
heytitle <pat.chormai@gmail.com>
2016-01-13 23:42:27 +0100

    Refactor code

--- app/scripts/lib/HamSandwich.js
+++ app/scripts/lib/HamSandwich.js
@@ -5,37 +5,36 @@ function HamSandwich(set1, set2) {
 
 HamSandwich.prototype.findMedian = function(lines) {
 	events = [new Point(Number.MIN_VALUE, Number.MIN_VALUE)];
-	for (i = 0; i < lines.length - 1; ++i) {
-		for (j = i + 1; j < lines.length; ++j) {
-			m1 = lines[i].m;
-			c1 = lines[i].c;
-			m2 = lines[j].m;
-			c2 = lines[j].c;
-
-			x = (c2 - c1) / (m1 - m2);
-			y = m1 * x + c1;
-			events.push(new Point(x,y));
+	for (i = 0; i < lines.length - 1; i++) {
+		for (j = i + 1; j < lines.length; j++) {
+            var point = lines[i].intersectWithLine( lines[j] );
+			events.push(point);
 		}
 	}
 
-	pointCompare <- function(p1, p2) {
+	pointCompare = function(p1, p2) {
 		return p1.x - p2.x;
-	});
+	};
 
 	events.sort(pointCompare);
 
-	medianLines = [];
-	medianPoints = [];
-	for (Point p in events) {
-		intersections = [];
-		for (Line line in lines) {
+	var medianLines = [];
+	var medianPoints = [];
+	for ( var i = 0; i < events.length; i++) {
+        var p = events[i];
+		var intersections = [];
+
+        for( var j = 0; j < lines.length; j++ ) {
+            var line = lines[j];
 			intersectionY = line.m * p.x + line.c;
-			intersections.push(new Point(p.x, intersectionY);
+			intersections.push(new Point(p.x, intersectionY));
 		}
+
 		intersections.sort(pointCompare);
 		median = intersections[Math.round(intersections.length / 2)];
 
-		for (Line line in lines) {
+        for( var j = 0; j < lines.length; j++ ) {
+            var line = lines[j];
 			if (line.isPointOnLine(median)) {
 				if (medianLines.length == 0) {
 					x = Number.MIN_VALUE;
@@ -55,6 +54,6 @@ HamSandwich.prototype.findMedian = function(lines) {
 	lastMedianLine = medianLines[medianLines.length - 1];
 	lastX = Number.MAX_VALUE - 1000;
 	lastY = lastMedianLine.m * lastX + lastMedianLine.c;
-	medianPoints.push(new Point(lastX, lastY);
+	medianPoints.push(new Point(lastX, lastY));
 	return medianPoints;
 }
