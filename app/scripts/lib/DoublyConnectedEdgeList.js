816b39bc529e54e69b3c008b0d51593a6821e550
Duy Pham <pham.duy89@gmail.com>
2016-01-23 22:44:47 +0100

    DCEL skeleton

---
+++ app/scripts/lib/DoublyConnectedEdgeList.js
@@ -0,0 +1,22 @@
+function HalfEdge() {
+	Point target;
+	Face incidentFace;
+
+	HalfEdge nextEdge;
+	HalfEdge previousEdge;
+	HalfEdge twin;
+}
+
+function Vertex() {
+	Point coordinate;
+	HalfEdge halfEdgeFromV;
+}
+
+function Face() {
+	HalfEdge HalfEdges;
+}
+
+function DCEL() {
+	Edge initialEdge;
+}
+
